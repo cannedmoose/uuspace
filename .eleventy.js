49cf55f0daadc30c00ea771b9df91fb71374897f
Callan McNamara <callan.mcnamara@gmail.com>
2020-09-22 12:14:07 +1000

    change to docs for easier hosting

--- .eleventy.js
+++ .eleventy.js
@@ -37,7 +37,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "./templates", // Equivalent to Jekyll's source property
-      output: "./_site", // Equivalent to Jekyll's destination property
+      output: "./docs", // Equivalent to Jekyll's destination property
     },
   };
 };
